40bdd82629f32c6ecc41f77740ebfa12add11a18
KemperKrasno <91101127+KemperKrasno@users.noreply.github.com>
2021-12-02 06:23:23 +0100

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+class MoneyConverter:
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозмодно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать колличество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = json.loads(r.content)[keys[quote]]
+
+        return amount*total_base
